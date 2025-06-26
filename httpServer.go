ae6b642d3abbc01d8db8006f2acb95e5dee16f04
alexiskov <alexiskov.rus@gmail.com>
2025-06-26 09:42:27 +0300

    comments

--- httpServer.go
+++ httpServer.go
@@ -2,6 +2,7 @@ package httpserver
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 )
 
@@ -11,18 +12,28 @@ type (
 		*Pipe
 	}
 	Pipe struct {
-		ErorLog *chan error
+		ErorLog *chan error //канал вывода лога ошибок из пакета
 	}
 )
 
+// принимает аргументом указатель на структуру содержащую каналы
+// возвращает указатель на структуру данных server
 func New(pipe *Pipe) *server {
 	return &server{&http.Server{Addr: ":8080"}, pipe}
 }
 
+// метод запускает сервер
+// если возникает ошибка - отправляет в канал ErorLog и завершает работу
 func (srv *server) Run() {
 	http.HandleFunc("/", srv.handlerMain)
+	if err := srv.ListenAndServe(); err != nil {
+		err = fmt.Errorf("http server runing error: %w", err)
+		*srv.Pipe.ErorLog <- err
+		return
+	}
 }
 
+// базовый хендлер
 func (srv *server) handlerMain(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -39,6 +50,7 @@ func (srv *server) handlerMain(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// обработчики методов
 func (srv *server) methodGET()    {}
 func (srv *server) methodPOST()   {}
 func (srv *server) methodPUT()    {}
